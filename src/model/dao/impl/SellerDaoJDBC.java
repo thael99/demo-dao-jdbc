9a5c0eb693008262e3467ae721af714a75b2eeae
Thael <thaelzaruz@gmail.com>
2026-06-11 18:34:49 -0300

    implementação do método findByDepartment

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -18,24 +21,6 @@ public class SellerDaoJDBC implements SellerDao{
 		this.conn = conn;
 	}
 	
-	@Override
-	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void update(Seller obj) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	@Override
 	public Seller findByID(Integer id) {
 		PreparedStatement st = null;
@@ -65,10 +50,27 @@ public class SellerDaoJDBC implements SellerDao{
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
-		
-		
+	}
+	
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
 	private Department instantiateDepartment(ResultSet rs) throws SQLException{
 		Department dep = new Department ();
@@ -94,6 +96,54 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("select seller.*, department.Name as DepName "+
+					"from seller inner join department "+
+					"on Seller.DepartmentId = department.Id "+
+					"where departmentId = ? " +
+					"Order by Name ");
+			st.setInt(1, department.getId());
+			rs = st.executeQuery(); 
+			List <Seller> list = new ArrayList<>();
+			/*    //jeito do professor
+			 Map <Integer, Department> map = new HashMap<>();
+			while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+				
+			}
+			*/
+			
+			//meu jeito
+			if (rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				do {
+					Seller obj = instantiateSeller(rs, dep);
+					list.add(obj);
+				}while (rs.next());
+			}
+
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
